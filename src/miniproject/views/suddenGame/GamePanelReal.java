f72ca7e9616d9ce8af7b633958cd63dcadc64275
KIMJH <KIMJH@ONIMONI>
2020-06-26 01:12:56 +0900

    added button links to MainMap.

--- src/miniproject/views/suddenGame/GamePanelReal.java
+++ src/miniproject/views/suddenGame/GamePanelReal.java
@@ -3,13 +3,18 @@ package miniproject.views.suddenGame;
 import java.awt.Graphics;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
+
+import practice.suddenGame.ChangePanel;
 
 public class GamePanelReal extends JPanel {
 	private Banner banner = new Banner();
 	private Gamepan pan = new Gamepan();
 	private GamePanelReal panel = this;
+	private JButton tempPass = new JButton("Submit");
 	
 	public void paintComponent(Graphics g) {
 		ImageIcon bground = new ImageIcon("suddenGame/bground_0.jpg");
@@ -18,11 +23,21 @@ public class GamePanelReal extends JPanel {
 		super.paintComponent(g);
 	}
 	
-	public GamePanelReal () {
+	public GamePanelReal (MainFrame mf) {
 		pan.setLocation(30,30);
 		this.add(pan);
 		
-
+tempPass.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ChangePanel.changePanel(mf, panel, /*새로운 메인맵*/);
+			}
+			
+		});
+			tempPass.setLayout(null);
+			tempPass.setBounds(1000,50,100,20);
+			this.add(tempPass);
 		
 	}
 }
