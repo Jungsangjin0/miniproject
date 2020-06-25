d45457bcaa3fb892c1760003e4bc5db1457ac88c
dongyub-lee <dongyub010@naver.com>
2020-06-25 17:09:40 +0900

    패널 추가 고생하시오

---
+++ src/dongyub/MyKeyBoardAdapter.java
@@ -0,0 +1,25 @@
+package dongyub;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class MyKeyBoardAdapter extends KeyAdapter {
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
+	}
+
+//	@Override
+//	public void keyPressed(KeyEvent e) {
+//		
+//		
+//	}
+//
+//	@Override
+//	public void keyReleased(KeyEvent e) {
+//		
+//		
+//	}
+
+}
---
+++ src/dongyub/MyMouseAdapter.java
@@ -0,0 +1,61 @@
+package dongyub;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import views.ChangePanel;
+import views.Dialog_dispatch;
+import views.Dispatch;
+import views.Dispatch_Game;
+import views.MainFrame;
+
+public class MyMouseAdapter extends MouseAdapter{
+
+	MainFrame mf;
+	Dispatch dis_panel;
+	Dispatch_Game panel;
+//	@Override
+//	public void mouseClicked(MouseEvent e) {
+//		//this.display("Mouse clicked (# of clicks : " + e.getClickCount() + " )", e);
+//		
+//		ChangePanel cp = new ChangePanel(mf, dis_panel);
+//		cp.changePaneDialog(panel);
+//		
+//	}
+	
+//	//누르는 순간에 동작
+//	@Override
+//	public void mousePressed(MouseEvent e) {
+//		
+//	}
+//	
+//	//눌렀다가 떴을때 동작
+//	@Override
+//	public void mouseReleased(MouseEvent e) {
+//		
+//	}
+//	
+//	//화면에 마우스가 들어왔을 때 
+//	@Override
+//	public void mouseEntered(MouseEvent e) {
+//	
+//	}
+//	
+//	//화면에서 마우스가 나갔을 때
+//	@Override
+//	public void mouseExited(MouseEvent e) {
+//		
+//	}
+//	
+//	
+//	@Override
+//	public void mouseDragged(MouseEvent e) {
+//		
+//		
+//	}
+//	@Override
+//	public void mouseMoved(MouseEvent e) {
+//	
+//		
+//	}
+}
